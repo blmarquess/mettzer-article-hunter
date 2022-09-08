21b5dcc39be8613eebde95ed5880ddbf0c204407
Bruno Marques <blmarques.dev@gmail.com>
2022-09-08 11:29:36 -0300

    feat(data-fetch): add to management cache and fetching data

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './services/http'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
 
