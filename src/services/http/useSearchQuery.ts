4fa868f589c1aaa65d8cebc79c33c597a2c4bbcd
Bruno Marques <blmarques.dev@gmail.com>
2022-09-09 18:34:16 -0300

    refactor(query-search): refactor return of data search

--- src/services/http/useSearchQuery.ts
+++ src/services/http/useSearchQuery.ts
@@ -3,16 +3,49 @@ import { searchApiV2 } from './httpClient'
 
 const createSearchKey = (key: string): QueryKey => [key]
 
+interface article {
+  id: number
+  title: string
+  authors: string
+  type: string
+  description: string
+  fulltextUrls: string
+}
+
+interface responseApi {
+  status: string
+  totalHits: number
+  data: article[]
+}
+const initialData = { isFetching: false, data: [], totalRows: 0 }
+
 export const useSearchQuery = (searchArticle: string) => {
-  const { isLoading, data, error, isFetching } = useQuery(
+  const { data, isFetching } = useQuery<responseApi>(
     createSearchKey(searchArticle),
     async () => await searchApiV2(searchArticle),
+    { staleTime: 0 },
   )
 
-  return {
-    isFetching,
-    isLoading,
-    data,
-    error,
+  if (searchArticle === '') {
+    return initialData
   }
+
+  const formatteddata =
+    data?.data.map(({ id, authors, title, description, fulltextUrls }: article) => ({
+      id,
+      title,
+      authors,
+      type: 'article',
+      description,
+      urls: fulltextUrls,
+    })) ?? []
+
+  const totalRows = data?.totalHits ?? 0
+
+  const resultes = {
+    isFetching,
+    data: formatteddata ?? [],
+    totalRows,
+  }
+  return resultes
 }
