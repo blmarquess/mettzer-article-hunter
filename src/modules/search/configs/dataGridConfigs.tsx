1f21673ba3febcdb4761bdcb17d3d19e9d70598d
Bruno Marques <blmarques.dev@gmail.com>
2022-09-09 19:33:33 -0300

    feat(data-grid): add buttons to open article in new window

--- src/modules/search/configs/dataGridConfigs.tsx
+++ src/modules/search/configs/dataGridConfigs.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@mui/material'
+import { GridRenderCellParams } from '@mui/x-data-grid'
+import { article } from 'domain/entities/article'
+
+const NOT_URL = '#'
+
 export const columns = [
   { field: 'id', headerName: 'ID', width: 80 },
   { field: 'title', headerName: 'Titulo', width: 250 },
@@ -5,4 +11,24 @@ export const columns = [
   { field: 'type', headerName: 'Tipo', width: 80 },
   { field: 'description', headerName: 'Descrição', width: 300 },
   { field: 'urls', headerName: 'URL', width: 230 },
+  {
+    field: 'Open',
+    headerName: 'Open',
+    renderCell: (params: GridRenderCellParams<article>) => {
+      console.log(params)
+      return (
+        <Button
+          onClick={() => window.open(`${(params.row?.urls[1] as string) ?? NOT_URL}`, '_blank')}>
+          Open
+        </Button>
+      )
+    },
+    width: 100,
+  },
+  {
+    field: 'Favoritar',
+    headerName: 'Favoritar',
+    renderCell: () => <Button>Favoritar</Button>,
+    width: 100,
+  },
 ]
