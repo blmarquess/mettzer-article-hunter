8b93ff11cc5bfb995b13c23f73f2b1ddc4ef819f
Bruno Marques <blmarques.dev@gmail.com>
2022-09-10 08:48:02 -0300

    fix(bug): fix type of id on remove item

--- src/services/repository/localStorage/removeFavorite.ts
+++ src/services/repository/localStorage/removeFavorite.ts
@@ -3,6 +3,6 @@ import { saveDataLS } from './saveData'
 
 export function removeFavorite(id: number) {
   const dbFavorite = readDataLS()
-  const newDB = dbFavorite.filter((item) => item.id !== id)
+  const newDB = dbFavorite.filter((item) => Number(item.id) !== Number(id))
   return saveDataLS(newDB)
 }
