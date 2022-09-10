c9b2be04d871781936e8847afccb8ca32c401f23
Bruno Marques <blmarques.dev@gmail.com>
2022-09-10 20:50:30 -0300

    test(ids): include data-test-ids to improvement tests

--- src/modules/search/components/IsFavorite.tsx
+++ src/modules/search/components/IsFavorite.tsx
@@ -9,13 +9,13 @@ export const IsFavorite = (props: article) => {
   const handleAddFavorite = () => addFavorite(props)
   if (isFavorite) {
     return (
-      <Button onClick={handleRemoveFavorite}>
+      <Button onClick={handleRemoveFavorite} data-testid={`favorite-${props.id}`}>
         <Favorite sx={{ color: 'yellow', height: 32, width: 32 }} stroke={'black'} />
       </Button>
     )
   }
   return (
-    <Button onClick={handleAddFavorite} variant="text">
+    <Button onClick={handleAddFavorite} variant="text" data-testid={`unfavorite-${props.id}`}>
       <Unfavorite sx={{ height: 32, width: 32 }} />
     </Button>
   )
