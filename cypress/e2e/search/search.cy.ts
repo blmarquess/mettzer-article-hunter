523b1285ac447bc2fd20bd3b5a7fbb4d60c1583f
Bruno Marques <blmarques.dev@gmail.com>
2022-09-10 20:32:30 -0300

    test(search): migrate add mock on tests

--- cypress/e2e/search/search.cy.ts
+++ cypress/e2e/search/search.cy.ts
@@ -5,13 +5,18 @@ describe('Search/list and open article', () => {
     cy.visit('/')
   })
   it('should visible all buttons and input box to search', () => {
+    cy.MockRequest()
     getButtonSearch().should('be.visible')
     getButtonFavorites().should('be.visible')
     getInputSearch().should('be.visible')
+    cy.wait('@MockRequest')
   })
 
   it('should possible search any article', () => {
+    cy.MockSearch()
     getInputSearch().type('rust')
     getButtonSearch().click()
+    cy.wait('@MockSearch')
+    cy.contains('Rust & Joiner')
   })
 })
