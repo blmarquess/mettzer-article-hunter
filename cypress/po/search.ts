6f88706ba5236c8aaa5d4eb9113ee0845e509ec9
Bruno Marques <blmarques.dev@gmail.com>
2022-09-10 16:46:42 -0300

    test(po): create page objects of search page

---
+++ cypress/po/search.ts
@@ -0,0 +1,4 @@
+export const getButtonSearch = () => cy.get('.MuiButton-contained').should('have.text', 'search')
+export const getButtonFavorites = () =>
+  cy.get('.MuiButton-outlined').should('have.text', 'favoritos')
+export const getInputSearch = () => cy.get('[data-testid="search-input"]')
