8b59e3819087cae8d8c111a6d86cc05e91eaad7d
Bruno Marques <blmarques.dev@gmail.com>
2022-09-10 08:49:27 -0300

    feat(favorite-btn): add button of go to favorite page

--- src/modules/search/configs/dataGridConfigs.tsx
+++ src/modules/search/configs/dataGridConfigs.tsx
@@ -2,6 +2,7 @@ import { Button } from '@mui/material'
 import { GridRenderCellParams } from '@mui/x-data-grid'
 import { article } from 'domain/entities'
 import { Link } from 'react-router-dom'
+import { IsFavorite } from '../components/IsFavorite'
 
 export const columns = [
   { field: 'id', headerName: 'ID', width: 150 },
@@ -11,13 +12,19 @@ export const columns = [
   { field: 'description', headerName: 'Descrição', width: 300 },
   { field: 'urls', headerName: 'URL', width: 230 },
   {
-    field: 'Open',
-    headerName: 'Open',
+    field: 'view',
+    headerName: 'Visualizar',
     renderCell: (params: GridRenderCellParams<article>) => (
       <Button component={Link} to={`/article/${params.id as string}`}>
-        Open
+        visualizar
       </Button>
     ),
+    width: 100,
+  },
+  {
+    field: 'favorite',
+    headerName: 'Favoritos',
+    renderCell: (params: GridRenderCellParams<article>) => <IsFavorite {...params.row} />,
     width: 100,
   },
 ]
