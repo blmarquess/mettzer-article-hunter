599fe95330c92ddf8c970fb674ebee9187ae76e5
Bruno Marques <blmarques.dev@gmail.com>
2022-09-10 20:54:37 -0300

    test(po): add page objects to favorite articles

--- cypress/po/search.ts
+++ cypress/po/search.ts
@@ -2,3 +2,6 @@ export const getButtonSearch = () => cy.get('.MuiButton-contained').should('have
 export const getButtonFavorites = () =>
   cy.get('.MuiButton-outlined').should('have.text', 'favoritos')
 export const getInputSearch = () => cy.get('[data-testid="search-input"]')
+export const getButtonViewArticle = (id) => cy.get(`[data-testid="article-${id}"]`)
+export const getButtonFavorite = (id) => cy.get(`[data-testid="favorite-${id}"]`)
+export const getButtonUnfavorite = (id) => cy.get(`[data-testid="unfavorite-${id}"]`)
