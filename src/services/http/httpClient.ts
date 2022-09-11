70e0b033c8b0491b90408e8b097371d7169e6e36
Bruno Marques <blmarques.dev@gmail.com>
2022-09-10 23:46:35 -0300

    docs(api): add example of environment variables for a test of the application

--- src/services/http/httpClient.ts
+++ src/services/http/httpClient.ts
@@ -2,13 +2,14 @@ import axios from 'axios'
 
 const URI = process.env.REACT_APP_ENDPOINT
 const API_PARAMS = process.env.REACT_APP_API_PARAMS as string
+const API_KEY = process.env.REACT_APP_API_KEY as string
 
 export const useHttpClient = axios.create({
   baseURL: URI,
 })
 
 export const searchArticles = async (str: string) =>
-  await useHttpClient.get(`/search/${str}${API_PARAMS}`)
+  await useHttpClient.get(`/search/${str}${API_PARAMS}${API_KEY}`)
 
 export const searchArticleById = async (id: string) =>
-  await useHttpClient.get(`/get/${id}${API_PARAMS}`)
+  await useHttpClient.get(`/get/${id}${API_PARAMS}${API_KEY}`)
