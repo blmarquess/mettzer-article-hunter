5f2e6c970e6dbe5aa19104204becf7434696c6e8
Bruno Marques <blmarques.dev@gmail.com>
2022-09-14 00:45:49 -0300

    docs(js-docs): add JSDocs to custom hooks

--- src/modules/search/hooks/useArticleQuery.ts
+++ src/modules/search/hooks/useArticleQuery.ts
@@ -1,19 +1,19 @@
 import { QueryKey, useQuery } from '@tanstack/react-query'
-import { AxiosResponse } from 'axios'
-import { article } from 'domain/entities'
+// import { AxiosResponse } from 'axios'
+// import { article } from 'domain/entities'
 import { searchArticleById } from 'services/http'
 
 const createSearchKey = (key: string): QueryKey => ['article', key]
-interface ArticleResponse {
-  data: article | AxiosResponse | undefined
-  isFetching: boolean
-  isError: boolean
-}
 
-export const useArticleQuery = (id: string): ArticleResponse => {
+/**
+ * @param {string} coreID  of core article id API ()
+ * @returns { data, isFetching, isError } return the article for coreID params.
+ * 'isFetching' and 'isError' are booleans to inform if has error or request in progress
+ */
+export const useArticleQuery = (coreID: string) => {
   const { data, isFetching, isError } = useQuery(
-    createSearchKey(id),
-    async () => await searchArticleById(id),
+    createSearchKey(coreID),
+    async () => await searchArticleById(coreID),
   )
   if (data?.status !== 'OK') {
     return { data, isFetching, isError }
--- src/modules/search/hooks/useSearchQuery.ts
+++ src/modules/search/hooks/useSearchQuery.ts
@@ -5,13 +5,18 @@ import { searchArticles } from 'services/http'
 
 const createSearchKey = (key: string, page = 0): QueryKey => [key, page]
 
+/**
+ * @param {string} searchArticle term to be searched in the API
+ * @param {number} pageSize number of articles per page per request
+ * @returns {article[], isFetching, isError} return one array of articles from search in API in key 'data'.
+ * 'isFetching' and 'isError' booleans to inform if has error or request in progress
+ */
 export const useSearchQuery = (searchArticle: string, pageSize: number) => {
   const [page, setPage] = useState(1)
   const { data, isFetching, isError } = useQuery(
     createSearchKey(searchArticle, page),
     async () => await searchArticles(searchArticle, page, pageSize),
   )
-
   const formattedData = data?.data
     ? data?.data.map(({ id, authors, title, description, fulltextUrls }: article) => ({
         id,
