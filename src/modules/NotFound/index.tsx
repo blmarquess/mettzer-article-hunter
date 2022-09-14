e0f0a5af35a8beb47abaa29f1cf2bf41473847c6
Bruno Marques <blmarques.dev@gmail.com>
2022-09-14 01:47:17 -0300

    feat(404): create page not found

--- src/modules/NotFound/index.tsx
+++ src/modules/NotFound/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Box } from '@mui/system'
+import ImgPageNoteFound from './images/404.png'
 
 export const NotFound = () => {
-  return <div>Page Not NotFound</div>
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '100Vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <img src={ImgPageNoteFound} alt="Pagina não encontrada" height="100%" />
+    </Box>
+  )
 }
 
 export default NotFound
